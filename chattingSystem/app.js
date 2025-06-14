3e8d08a31f618582311dc2468fbad5d469da6f83
blizzard65 <bobrossrossrosstoiletskib@gmail.com>
2025-06-14 07:00:35 -0700

    lets try this code, maybe I get to sleep

--- chattingSystem/app.js
+++ chattingSystem/app.js
@@ -1,7 +1,13 @@
-const supabase = SupabaseClient.createClient(
-  'https://vpbiokohfyslgudzesot.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwYmlva29oZnlzbGd1ZHplc290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4ODI2NDgsImV4cCI6MjA2NTQ1ODY0OH0.sQHNxJNnXe92QAf-yMmEJseWT1fblY0K3mC6NgbdwUw'
-);
+// const supabase = SupabaseClient.createClient(
+//   'https://vpbiokohfyslgudzesot.supabase.co',
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwYmlva29oZnlzbGd1ZHplc290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4ODI2NDgsImV4cCI6MjA2NTQ1ODY0OH0.sQHNxJNnXe92QAf-yMmEJseWT1fblY0K3mC6NgbdwUw'
+// );
+
+  const { createClient } = supabase
+  const _supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+
+  console.log('Supabase Instance: ', _supabase)
+  // ...
 
 const messagesElement = document.querySelector('#messages');
 
