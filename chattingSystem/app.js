b738958ee1346e92126d6f949b554b11605ffe6a
blizzard65 <bobrossrossrosstoiletskib@gmail.com>
2025-06-14 04:09:55 -0700

    ?

--- chattingSystem/app.js
+++ chattingSystem/app.js
@@ -1,6 +1,6 @@
 const supabase = SupabaseClient.createClient(
   'https://vpbiokohfyslgudzesot.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwYmlva29oZnlzbGd1ZHplc290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4ODI2NDgsImV4cCI6MjA2NTQ1ODY0OH0.sQHNxJNnXe92QAf-yMmEJseWT1fblY0K3mC6NgbdwUw',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwYmlva29oZnlzbGd1ZHplc290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4ODI2NDgsImV4cCI6MjA2NTQ1ODY0OH0.sQHNxJNnXe92QAf-yMmEJseWT1fblY0K3mC6NgbdwUw'
 );
 
 const messagesElement = document.querySelector('#messages');
